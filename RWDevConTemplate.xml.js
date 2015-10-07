e773b65c9578c4b4a0666da6c53cb215a8497715
NickCulbertson <nickculbertson@gmail.com>
2015-10-07 14:53:10 -0500

    Added Files
    Added Files

---
+++ RWDevConTemplate.xml.js
@@ -0,0 +1,148 @@
+var Template = function() { return `<?xml version="1.0" encoding="UTF-8" ?>
+  <document>
+    <catalogTemplate> 
+      <banner> 
+        <title>Free Classic Movies Public Domain Videos</title>
+      </banner>
+      <list> 
+        <section> 
+				  <listItemLockup> 
+				    <title>Public Domain Videos</title>
+				    <decorationLabel>18</decorationLabel>
+			            //1. add from here
+				    <relatedContent> 
+				      <grid>
+					  <section> 
+						
+						                  //2
+								  <lockup videoURL="https://archive.org/download/NightOfTheLivingDead720p1968/NightOfLivingDead720p.mp4">
+								    <img src="https://upload.wikimedia.org/wikipedia/commons/5/56/Zombies_NightoftheLivingDead.jpg" width="500" height="308" />
+											<title>Night of The Living Dead</title>
+								  </lockup>
+								  <lockup videoURL="https://archive.org/download/Plan_9_from_Outer_Space_1959/Plan_9_from_Outer_Space_1959.mp4">
+								    <img src="${this.BASEURL}2.png" width="500" height="308" />
+								  </lockup>									
+							          <lockup videoURL="https://archive.org/download/VoyagetothePlanetofPrehistoricWomen/VoyagetothePlanetofPrehistoricWomen_512kb.mp4">
+								    <img src="${this.BASEURL}3.png" width="500" height="308" />
+								  </lockup>									
+								  <lockup videoURL="https://archive.org/download/house_on_haunted_hill_ipod/house_on_haunted_hill_512kb.mp4">
+								    <img src="${this.BASEURL}4.png" width="500" height="308" />
+								  </lockup>									
+							          <lockup videoURL="https://archive.org/download/ThePhantomCreeps/ThePhantomCreeps_512kb.mp4">
+								    <img src="${this.BASEURL}5.png" width="500" height="308" />
+							          </lockup>									
+								  <lockup videoURL="https://archive.org/download/Nosferatu_most_complete_version_93_mins./Nosferatu_1922_Symphony_of_Horror_512kb.mp4">
+								    <img src="${this.BASEURL}6.png" width="500" height="308" />
+								  </lockup>									
+								  <lockup videoURL="https://archive.org/download/20000LeaguesUndertheSea/20000_Leagues_Under_the_Sea_512kb.mp4">
+								    <img src="${this.BASEURL}7.png" width="500" height="308" />
+							          </lockup>									
+								  <lockup videoURL="https://archive.org/download/LeVoyageDansLaLune_218/voyage.mp4">
+								    <img src="${this.BASEURL}8.png" width="500" height="308" />
+								  </lockup>									
+								  <lockup videoURL="https://archive.org/download/TheFastandtheFuriousJohnIreland1954goofyrip/TheFastandtheFuriousJohnIreland1954goofyrip_512kb.mp4">
+								    <img src="${this.BASEURL}9.png" width="500" height="308" />
+								  </lockup>									
+								  <lockup videoURL="https://archive.org/download/Return_of_the_Kung_Fu_Dragon/Return_of_the_Kung_Fu_Dragon.mp4">
+								    <img src="${this.BASEURL}10.png" width="500" height="308" />
+								  </lockup>									
+								  <lockup videoURL="https://archive.org/download/utopia/Utopia_512kb.mp4">
+								    <img src="${this.BASEURL}11.png" width="500" height="308" />
+								  </lockup>									
+							          <lockup videoURL="https://archive.org/download/SlowMotionFlame/slomoflame_512kb.mp4">
+								    <img src="${this.BASEURL}12.png" width="500" height="308" />
+								  </lockup>									
+								  <lockup videoURL="https://archive.org/download/charlie_chaplin_film_fest/charlie_chaplin_film_fest_512kb.mp4">
+								    <img src="${this.BASEURL}13.png" width="500" height="308" />
+								  </lockup>		
+								   <lockup videoURL="https://archive.org/download/bigamist_the/the_bigamist.mp4">
+								    <img src="${this.BASEURL}14.png" width="500" height="308" />
+								  </lockup>	
+								  <lockup videoURL="https://youtu.be/EGARXyj1ZBk">
+								    <img src="${this.BASEURL}15.png" width="500" height="308" />
+								  </lockup>	
+								  <lockup videoURL="http://streaming.radionomy.com/BestofJazz-LudwigRadiocom">
+								    <img src="${this.BASEURL}16.png" width="500" height="308" />
+								  </lockup>	
+								  <lockup videoURL="https://archive.org/download/adventures_holmes/adventureholmes_01_doyle.mp3">
+								    <img src="${this.BASEURL}17.png" width="500" height="308" />
+								  </lockup>	
+								  <lockup videoURL="https://archive.org/download/adventures_holmes/adventures_holmes_64kb.m3u">
+								    <img src="${this.BASEURL}18.png" width="500" height="308" />
+								  </lockup>	
+								</section>
+				      </grid>
+				    </relatedContent>
+				  </listItemLockup>
+				 <listItemLockup> 
+				    <title>Public Domain Videos</title>
+				    <decorationLabel>18</decorationLabel>
+			            //1. add from here
+				    <relatedContent> 
+				      <grid>
+				        <section> 
+						                  //2
+								  <lockup videoURL="https://archive.org/download/NightOfTheLivingDead720p1968/NightOfLivingDead720p.mp4">
+								    <img src="${this.BASEURL}1.png" width="500" height="308" />
+								  </lockup>
+								  <lockup videoURL="https://archive.org/download/Plan_9_from_Outer_Space_1959/Plan_9_from_Outer_Space_1959.mp4">
+								    <img src="${this.BASEURL}2.png" width="500" height="308" />
+								  </lockup>									
+							          <lockup videoURL="https://archive.org/download/VoyagetothePlanetofPrehistoricWomen/VoyagetothePlanetofPrehistoricWomen_512kb.mp4">
+								    <img src="${this.BASEURL}3.png" width="500" height="308" />
+								  </lockup>									
+								  <lockup videoURL="https://archive.org/download/house_on_haunted_hill_ipod/house_on_haunted_hill_512kb.mp4">
+								    <img src="${this.BASEURL}4.png" width="500" height="308" />
+								  </lockup>									
+							          <lockup videoURL="https://archive.org/download/ThePhantomCreeps/ThePhantomCreeps_512kb.mp4">
+								    <img src="${this.BASEURL}5.png" width="500" height="308" />
+							          </lockup>									
+								  <lockup videoURL="https://archive.org/download/Nosferatu_most_complete_version_93_mins./Nosferatu_1922_Symphony_of_Horror_512kb.mp4">
+								    <img src="${this.BASEURL}6.png" width="500" height="308" />
+								  </lockup>									
+								  <lockup videoURL="https://archive.org/download/20000LeaguesUndertheSea/20000_Leagues_Under_the_Sea_512kb.mp4">
+								    <img src="${this.BASEURL}7.png" width="500" height="308" />
+							          </lockup>									
+								  <lockup videoURL="https://archive.org/download/LeVoyageDansLaLune_218/voyage.mp4">
+								    <img src="${this.BASEURL}8.png" width="500" height="308" />
+								  </lockup>									
+								  <lockup videoURL="https://archive.org/download/TheFastandtheFuriousJohnIreland1954goofyrip/TheFastandtheFuriousJohnIreland1954goofyrip_512kb.mp4">
+								    <img src="${this.BASEURL}9.png" width="500" height="308" />
+								  </lockup>									
+								  <lockup videoURL="https://archive.org/download/Return_of_the_Kung_Fu_Dragon/Return_of_the_Kung_Fu_Dragon.mp4">
+								    <img src="${this.BASEURL}10.png" width="500" height="308" />
+								  </lockup>									
+								  <lockup videoURL="https://archive.org/download/utopia/Utopia_512kb.mp4">
+								    <img src="${this.BASEURL}11.png" width="500" height="308" />
+								  </lockup>									
+							          <lockup videoURL="https://archive.org/download/SlowMotionFlame/slomoflame_512kb.mp4">
+								    <img src="${this.BASEURL}12.png" width="500" height="308" />
+								  </lockup>									
+								  <lockup videoURL="https://archive.org/download/charlie_chaplin_film_fest/charlie_chaplin_film_fest_512kb.mp4">
+								    <img src="${this.BASEURL}13.png" width="500" height="308" />
+								  </lockup>		
+								   <lockup videoURL="https://archive.org/download/bigamist_the/the_bigamist.mp4">
+								    <img src="${this.BASEURL}14.png" width="500" height="308" />
+								  </lockup>	
+								  <lockup videoURL="https://youtu.be/EGARXyj1ZBk">
+								    <img src="${this.BASEURL}15.png" width="500" height="308" />
+								  </lockup>	
+								  <lockup videoURL="http://streaming.radionomy.com/BestofJazz-LudwigRadiocom">
+								    <img src="${this.BASEURL}16.png" width="500" height="308" />
+								  </lockup>	
+								  <lockup videoURL="https://archive.org/download/adventures_holmes/adventureholmes_01_doyle.mp3">
+								    <img src="${this.BASEURL}17.png" width="500" height="308" />
+								  </lockup>	
+								  <lockup videoURL="https://archive.org/download/adventures_holmes/adventures_holmes_64kb.m3u">
+								    <img src="${this.BASEURL}18.png" width="500" height="308" />
+								  </lockup>	
+								</section>
+				      </grid>
+				    </relatedContent>
+				  </listItemLockup>
+				  
+        </section>
+      </list>
+    </catalogTemplate>
+  </document>`
+}
